76614358bfcef5b1766c0d7aac4267d406acc191
magomed03 <magomed_mazhitov_03@mail.ru>
2024-06-24 15:18:26 +0300

    First practice

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -1,62 +1,41 @@
 package com.example.compose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var i = 1
+        val numbers = Array(100, {i++})
         enableEdgeToEdge()
         setContent {
-            Box(modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center){
-                CircleButton()
+            LazyColumn(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize().padding(top = 16.dp)
+            ) {
+                itemsIndexed(numbers) {_, item ->
+                    Text(
+                        text = "Item $item",
+                        fontSize = 20.sp,
+                        modifier = Modifier.padding(5.dp)
+                    )
+                    Log.d("MyLog", "$item")
+                }
             }
         }
     }
 }
 
-@Composable
-fun CircleButton() {
-    val counter = remember {
-        mutableIntStateOf(0)
-    }
-    val color = remember {
-        mutableStateOf(Color.Red)
-    }
-    Box(modifier = Modifier
-        .size(100.dp)
-        .background(color.value, shape = CircleShape)
-        .clickable {
-            when (++counter.intValue) {
-                10 -> color.value = Color.Blue
-                20 -> color.value = Color.Green
-            }
-        },
-        contentAlignment = Alignment.Center){
-        Text(text = counter.intValue.toString(),
-            style = TextStyle(color = Color.White, fontSize = 20.sp)
-        )
-    }
-
-
-}
