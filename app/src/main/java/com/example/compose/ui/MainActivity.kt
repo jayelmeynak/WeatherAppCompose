3b608c2a90cf7b2d85ff1290f6fcfa4a9dbd1ba4
magomed03 <magomed_mazhitov_03@mail.ru>
2024-06-26 02:24:40 +0300

    Created maincard and pager

--- app/src/main/java/com/example/compose/ui/MainActivity.kt
+++ app/src/main/java/com/example/compose/ui/MainActivity.kt
@@ -1,31 +1,18 @@
 package com.example.compose
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import org.json.JSONObject
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.compose.ui.MainCard
+import com.example.compose.ui.TabLayout
 
 const val API_KEY = "80f2a708a2fb49d7a04192911241503"
 
@@ -34,74 +21,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Weather("Moscow", this@MainActivity)
-            }
-        }
-    }
-}
-
-@Composable
-fun Weather(name: String, context: Context) {
-    val temp = remember {
-        mutableStateOf("Unknown")
-    }
-    Column(modifier = Modifier.fillMaxSize()) {
-        Box(
-            modifier = Modifier
-                .fillMaxHeight(0.5f)
-                .fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = "Temp in $name = ${temp.value}",
-                fontSize = 20.sp
+            Image(
+                painter = painterResource(id = R.drawable.weather_image),
+                contentDescription = "image",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .alpha(0.9f),
+                contentScale = ContentScale.FillBounds
             )
-        }
-        Box(
-            modifier = Modifier
-                .fillMaxHeight()
-                .fillMaxWidth(),
-            contentAlignment = Alignment.BottomCenter
-        ) {
-            Button(
-                onClick = {
-                    getResult(name, temp, context)
-                }, modifier = Modifier
-                    .padding(8.dp)
-                    .fillMaxWidth()
-            ) {
-                Text(
-                    text = "Refresh",
-                    fontSize = 20.sp
-                )
+            Column {
+                MainCard()
+                TabLayout()
             }
         }
     }
 }
 
-private fun getResult(city: String, state: MutableState<String>, context: Context) {
-    val url = "https://api.weatherapi.com/v1/current.json?key=$API_KEY&q=$city&aqi=no"
-    val queue = Volley.newRequestQueue(context)
-    val stringRequest = StringRequest(
-        Request.Method.GET,
-        url,
-        {
-            response -> state.value = parseWeather(response) + "°C"
-
-        },
-        {
-            error -> Log.d("MyLog", "$error")
-        }
-    )
-    queue.add(stringRequest)
-}
-
-private fun parseWeather(result: String) : String{
-    val mainObject = JSONObject(result)
-    val temp = mainObject.getJSONObject("current").getString("temp_c")
-    return temp
-}
