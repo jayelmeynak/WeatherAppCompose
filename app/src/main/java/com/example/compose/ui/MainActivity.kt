958e50d008cb550be94dc17d1432d5b094eb0c72
jayelmeynak <magomed110301@yandex.ru>
2024-06-30 20:01:38 +0300

    Create TopAppBar and SnackBar

--- app/src/main/java/com/example/compose/ui/MainActivity.kt
+++ app/src/main/java/com/example/compose/ui/MainActivity.kt
@@ -1,23 +1,92 @@
 package com.example.compose
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import com.example.compose.ui.MainScreen
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarResult
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.compose.ui.theme.ComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             ComposeTheme {
-                MainScreen()
+                MainScreen(context = applicationContext)
             }
         }
     }
 }
 
+
+@Composable
+fun MainScreen(context: Context) {
+    val coroutineScope = rememberCoroutineScope()
+    val scaffoldState = rememberScaffoldState()
+
+    Scaffold(
+        modifier = Modifier.systemBarsPadding(),
+        scaffoldState = scaffoldState,
+        topBar = {
+            TopAppBar(
+                backgroundColor = Color.Gray,
+                title = {
+                    Text(text = "Menu")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        Toast.makeText(context, "Menu", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {
+                        coroutineScope.launch {
+                            val result = scaffoldState.snackbarHostState.showSnackbar(
+                                message = "Item deleted",
+                                actionLabel = "Undone"
+                            )
+                            if (result == SnackbarResult.ActionPerformed){
+                                Toast.makeText(context, "Item recovered", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }) {
+                        Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
+                    }
+                    IconButton(onClick = {
+                        Toast.makeText(context, "Done", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Icon(imageVector = Icons.Default.Done, contentDescription = "Done")
+                    }
+                }
+            )
+        }
+    ) {
+        Text(
+            modifier = Modifier.padding(top = it.calculateTopPadding() + 16.dp),
+            text = "Hello")
+    }
+}
 
 
